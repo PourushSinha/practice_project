e34301b365db0cd8a97e74c3738bebe6344b7491
Pourush Sinha <pourush.sinha@olacabs.com>
2018-12-22 23:44:29 +0530

    DP practice

---
+++ algorithm/src/main/java/utility/CommonUtils.java
@@ -0,0 +1,12 @@
+package utility;
+
+import java.util.Arrays;
+
+/**
+ * Created by pourush.sinha on 22/12/18.
+ */
+public class CommonUtils {
+
+    public static void printArray(int[] arr) {
+        System.out.println(Arrays.toString(arr)); }
+}
